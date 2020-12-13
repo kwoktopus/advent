1b1669cc8137b1910ed43a92f209ae13b124c320
Andy Wang <z5161179@student.unsw.edu.au>
2020-12-13 17:45:31 +1100

    day 6

--- 06/p1.cpp
+++ 06/p1.cpp
@@ -3,11 +3,28 @@
 #include <cstring>
 #include <algorithm>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 int main(void) {
+	string s;
+	unordered_set<char> set;
+	int ans = 0;
+	while (cin) {
+		getline(cin, s);
 
+		if (s.empty()) {
+			ans += set.size();
+			set.clear();
+		} else {
+			for (char c : s) {
+				set.insert(c);
+			}
+		}
+	} 
+
+	cout << ans + set.size() << '\n';
 
 	return 0;
 }
--- 06/p2.cpp
+++ 06/p2.cpp
@@ -7,7 +7,33 @@
 using namespace std;
 
 int main(void) {
+	string s;
+	int ans = 0, n = 0;
+	int answers[26];
+	memset(answers, 0, sizeof(answers));
 
+	while (cin) {
+		getline(cin, s);
+
+		if (s.empty()) {
+			for (int i = 0; i < 26; i++) {
+				if (answers[i] == n) ans ++;
+			}
+			n = 0;
+			memset(answers, 0, sizeof(answers));
+		} else {
+			n++;
+			for (char c : s) {
+				answers[c - 'a'] ++;
+			}
+		}
+	} 
+
+	for (int i = 0; i < 26; i++) {
+		if (answers[i] == n) ans ++;
+	}
+
+	cout << ans << '\n';
 
 	return 0;
 }
