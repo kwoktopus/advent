7d62886f75cc4e94e749eb67535a27f812817f53
Andy Wang <z5161179@student.unsw.edu.au>
2020-12-13 21:56:45 +1100

    day 10

--- 10/p1.cpp
+++ 10/p1.cpp
@@ -7,7 +7,27 @@
 using namespace std;
 
 int main(void) {
+	int n;
+	vector<int> v;
 
+	while (cin >> n) {
+		v.push_back(n);
+	}
 
-	return 0;
+	sort(v.begin(), v.end());
+
+	int prev = 0;
+	int one = 0;
+	int three = 1;
+
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] - prev == 1) one ++;
+		else if (v[i] - prev == 3) three ++;
+
+		prev = v[i];		
+	}
+
+	cout << one << " * " << three << " = " << one*three << "\n";
+
+	return 0;	
 }
--- 10/p2.cpp
+++ 10/p2.cpp
@@ -6,8 +6,44 @@
 
 using namespace std;
 
+typedef long long ll;
+
+
 int main(void) {
 
+	int n;
+	vector<int> v;
 
-	return 0;
+	while (cin >> n) {
+		v.push_back(n);
+	}
+
+	v.push_back(0);
+	sort(v.begin(), v.end());
+	v.push_back(v[v.size() - 1] + 3);
+
+	n = v.size();
+
+	// dp[i][j] = number of ways to reach power[j] using the first j adapters	
+	ll dp[n];
+	memset(dp, 0, sizeof(dp));
+	dp[0] = 1;
+
+	for (int i = 1; i < n; i++) {
+		ll val = v[i];
+
+		for (int j = i - 1; j >= 0; j--) {
+			if (val - v[j] > 3) break;
+		 	dp[i] += dp[j];
+		}
+	}
+
+	// for (int i = 0; i < n; i++) {
+	// 	cout << dp[i] << "\n";
+	// }
+
+
+	cout << dp[n - 1] << "\n";
+
+	return 0;	
 }
