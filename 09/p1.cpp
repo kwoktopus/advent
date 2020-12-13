ae2143c3a20f93239f12e601b2d4e20580f0e83c
Andy Wang <z5161179@student.unsw.edu.au>
2020-12-13 20:38:58 +1100

    day 9

--- 09/p1.cpp
+++ 09/p1.cpp
@@ -6,8 +6,33 @@
 
 using namespace std;
 
-int main(void) {
+typedef long long ll;
+const int N = 25;
 
+int main(void) {
+	vector<ll> v;
+
+	ll n;
+	while (cin >> n) {
+		v.push_back(n);
+	}
+
+	for (int i = N; i < v.size(); i++) {
+		bool valid = false;
+		for (int j = i - N; j < i; j++) {
+			for (int k = j + 1; k < i; k++) {
+				if (v[j] != v[k] && v[j] + v[k] == v[i]) {
+					valid = true;
+					break;
+				}
+			}
+		}
+
+		if (!valid) {
+			cout << v[i] << "\n";
+			break;
+		}
+	}
 
 	return 0;
 }
--- 09/p2.cpp
+++ 09/p2.cpp
@@ -6,8 +6,54 @@
 
 using namespace std;
 
-int main(void) {
+typedef long long ll;
+const int N = 25;
 
+int main(void) {
+	vector<ll> v;
+
+	ll n;
+	int idx;
+	while (cin >> n) {
+		v.push_back(n);
+	}
+
+	for (int i = N; i < v.size(); i++) {
+		bool valid = false;
+		for (int j = i - N; j < i; j++) {
+			for (int k = j + 1; k < i; k++) {
+				if (v[j] != v[k] && v[j] + v[k] == v[i]) {
+					valid = true;
+					break;
+				}
+			}
+		}
+
+		if (!valid) {
+			idx = i;
+			break;
+		}
+	}
+
+	idx--;
+	for (int i = 0; i < idx; i++) {
+		ll sum = 0;
+
+		ll smallest = v[i];
+		ll largest = v[i];
+
+		for (int j = i; j < idx; j++) {
+			sum += v[j];
+
+			smallest = min(smallest, v[j]);
+			largest = max(largest, v[j]);
+
+			if (sum == v[idx + 1]) {
+				cout << smallest + largest << "\n";
+				return 0;
+			}
+		}
+	}
 
 	return 0;
 }
