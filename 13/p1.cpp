bef29fad5350631b342da3e965131ee11e43137c
Andy Wang <z5161179@student.unsw.edu.au>
2020-12-13 23:53:39 +1100

    day 13

--- 13/p1.cpp
+++ 13/p1.cpp
@@ -8,6 +8,33 @@ using namespace std;
 
 int main(void) {
 
+	int n;
+	string s;
+	cin >> n >> s;
+
+	vector<int> times;
+	s += ',';
+
+	int idx = -1;
+	while ((idx = s.find(",")) != -1) {
+		string sub = s.substr(0, idx);
+		s.erase(0, idx + 1);
+
+		if (sub != "x") times.push_back(stoi(sub));
+	}
+	int best = 123456789;
+	int id = -1;
+
+	for (int t : times) {
+		int departTime = ceil(1.0*n/t) * t;
+
+		if (departTime - n < best) {
+			best = departTime - n;
+			id = t;
+		}
+	}
+
+	cout << best * id << "\n";
 
 	return 0;
 }
--- 13/p2.cpp
+++ 13/p2.cpp
@@ -6,7 +6,66 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main(void) {
+
+	int n;
+	string s;
+	cin >> n >> s;
+
+	vector<pair<ll, int>> times;
+
+	s += ',';
+	int id = 0;
+	int idx = -1;
+	while ((idx = s.find(",")) != -1) {
+		string sub = s.substr(0, idx);
+		s.erase(0, idx + 1);
+
+		if (sub != "x") times.push_back({stoi(sub), id});
+		id++;
+	}
+	n = times.size();
+
+
+
+
+	ll multiple = times[0].first;
+	ll constant = 0;
+	ll ans;
+	
+	for (int a = 2; a <= n; a++) {
+		ll k = 0;
+		while (true) {
+			ll time = multiple*k + constant;
+			ll prev = time;
+			ll valid = true;
+			// cout << "testing: " << time << "const = " << constant << " mult = " << multiple << '\n';
+
+			for (int i = 1; i < a; i++) {
+				// get the departure time
+				ll targetTime = time + times[i].second;
+
+				if (targetTime%times[i].first) {
+					valid = false; 
+					break;
+				}
+			}
+
+			if (valid) {
+				multiple *= times[a - 1].first;
+				constant = time;
+				ans = time;
+				cout << time << " " << multiple << '\n';
+				break;
+			}
+
+			k++;
+		}
+	}
+
+	cout << "\nFINAL ANSWER:\n" << ans << '\n';
 
 
 	return 0;
