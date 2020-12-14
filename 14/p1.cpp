86c2325b726a8a0296068e12a3c830cd12775b4d
Andy Wang <z5161179@student.unsw.edu.au>
2020-12-14 19:16:50 +1100

    day 14

--- 14/p1.cpp
+++ 14/p1.cpp
@@ -3,11 +3,51 @@
 #include <cstring>
 #include <algorithm>
 #include <vector>
+#include <bitset>
+#include <unordered_map>
 
 using namespace std;
+const int N = 36;
+typedef long long ll;
 
 int main(void) {
+	string mask;
+	cin >> mask >> mask >> mask;
 
+
+	ll n = mask.length();
+	unordered_map<int,ll> mem;
+
+	string a,b,c;
+	while (cin >> a >> b >> c) {
+
+		if (a == "mask") {
+			mask = c;
+			continue;
+		}
+		int idx = stoi(a.substr(4, a.length() - 1));
+		ll num = stoi(c);
+
+		// cout << idx << " - " << num << "\n";
+
+		bitset<N> bs(num);
+
+		for (int i = n - 1; i >= 0; i--) {
+			if (mask[i] == 'X') continue;
+
+			bs[n - 1 - i] = mask[i] - '0';
+		}
+
+		mem[idx] = bs.to_ulong();
+	}
+
+	ll ans = 0;
+	for (auto it : mem) {
+		// cout << it.first << " -> " << it.second << '\n';
+		ans += it.second;
+	}
+
+	cout << ans << "\n";
 
 	return 0;
 }
--- 14/p2.cpp
+++ 14/p2.cpp
@@ -3,11 +3,84 @@
 #include <cstring>
 #include <algorithm>
 #include <vector>
+#include <bitset>
+#include <unordered_map>
 
 using namespace std;
+const int N = 36;
+typedef long long ll;
+
+vector<string> bitsets;
+
+void recurse(string s, int idx) {
+	if (idx == s.length()) {
+		bitsets.push_back(s);
+		return;
+	}
+
+	if (s[idx] == 'X') {
+		s[idx] = '1';
+		recurse(s, idx + 1);
+
+		s[idx] = '0';
+		recurse(s, idx + 1);
+		
+		return;
+	}
+
+	recurse(s, idx + 1);
+}
 
 int main(void) {
+	string mask;
+	cin >> mask >> mask >> mask;
 
+
+	ll n = mask.length();
+	unordered_map<ll,ll> mem;
+
+	string a,b,c;
+	while (cin >> a >> b >> c) {
+
+		if (a == "mask") {
+			mask = c;
+			continue;
+		}
+
+		ll idx = stoi(a.substr(4, a.length() - 1));
+		ll num = stoi(c);
+
+		// cout << idx << " - " << num << "\n";
+		// idx gets modified instead
+		bitset<N> bs(idx);
+		string bstring = bs.to_string();
+		for (int i = 0; i < n; i++) {
+			if (mask[i] == '0') continue;
+			else bstring[i] = mask[i];
+		}
+
+
+		bitsets.clear();
+		recurse(bstring, 0);
+
+		for (string out : bitsets) {
+			bitset<N> idxbs;
+			for (int i = 0; i < n; i++) {
+				idxbs[n - 1 - i] = out[i] - '0';
+			}
+
+			mem[idxbs.to_ulong()] = num;
+		}
+
+	}
+
+	ll ans = 0;
+	for (auto it : mem) {
+		// cout << it.first << " -> " << it.second << '\n';
+		ans += it.second;
+	}
+
+	cout << ans << "\n";
 
 	return 0;
 }
